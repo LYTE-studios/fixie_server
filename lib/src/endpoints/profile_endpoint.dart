7ba2e03ed8947d5c8d733b4f0aab5ed5d2dc2566
Tanguy De Brabandere <125645329+TanguyDeBrabandere@users.noreply.github.com>
2024-03-28 16:16:30 +0100

    Update profile_endpoint.dart

--- lib/src/endpoints/profile_endpoint.dart
+++ lib/src/endpoints/profile_endpoint.dart
@@ -1,18 +1,20 @@
 import 'dart:io';
-import 'dart:js_interop';
 
 import 'package:fixie_server/src/generated/protocol.dart';
 import 'package:serverpod/serverpod.dart';
 import 'package:serverpod_auth_server/module.dart';
 
 class ProfileEndpoint extends Endpoint {
-  Future<UserProfile> getProfileData(Session session) async { 
+  Future<UserProfile> getProfileData(Session session) async {
     int? authenticatedUserId = await session.auth.authenticatedUserId;
-    UserInfo? user = await UserInfo.db.findById(session,authenticatedUserId!);
+    UserInfo? user = await UserInfo.db.findById(session, authenticatedUserId!);
 
     if (user != null) {
       int daysSinceCreation = DateTime.now().difference(user.created).inDays;
-      UserProfile userProfileData = UserProfile(name: user.fullName!, email: user.email!, daysSinceCreation: daysSinceCreation);
+      UserProfile userProfileData = UserProfile(
+          name: user.fullName!,
+          email: user.email!,
+          daysSinceCreation: daysSinceCreation);
       return userProfileData;
     } else {
       throw Exception("User is null");
@@ -28,7 +30,7 @@ class ProfileEndpoint extends Endpoint {
     if (user != null) {
       user.birthday = birthday;
       var updatedUser = await User.db.updateRow(session, user);
-      if(updatedUser.birthday.isAtSameMomentAs(birthday)) {
+      if (updatedUser.birthday.isAtSameMomentAs(birthday)) {
         return HttpStatus.ok;
       } else {
         return HttpStatus.notModified;
@@ -48,7 +50,7 @@ class ProfileEndpoint extends Endpoint {
       user.fullName = name;
       var updatedUser = await UserInfo.db.updateRow(session, user);
       var fullName = updatedUser.fullName;
-      if(fullName != null && fullName == name) {
+      if (fullName != null && fullName == name) {
         return HttpStatus.ok;
       } else {
         return HttpStatus.notModified;
