0250ba6490e8f67e338e69173963d42f86c4003d
Tanguy De Brabandere <125645329+TanguyDeBrabandere@users.noreply.github.com>
2024-07-11 13:56:19 +0200

    Update run_server.sh

--- run_server.sh
+++ run_server.sh
@@ -8,22 +8,9 @@ sudo cp nginx.conf /etc/nginx/nginx.conf
 sudo cp certificate.crt /etc/nginx/certificate.crt
 sudo cp private.key /etc/nginx/private.key
 
-sudo rm /etc/systemd/system/fixie.socket
-sudo rm /etc/systemd/system/fixie.service
-
-sudo touch /etc/systemd/system/fixie.socket
-sudo touch /etc/systemd/system/fixie.service
-
-
-sudo cp fixie.socket /etc/systemd/system/fixie.socket
-sudo cp fixie.service /etc/systemd/system/fixie.service
-
 sudo systemctl daemon-reload
-
-sudo systemctl start fixie.socket
-sudo systemctl start fixie.service
-
-sudo systemctl status fixie.socket
 
 sudo systemctl restart nginx
 sudo systemctl status nginx
+
+docker build .
