dffa830757a4a1fa2d22f8a754e6af6d60237dae
Tanguy De Brabandere <125645329+TanguyDeBrabandere@users.noreply.github.com>
2024-07-11 13:03:56 +0200

    fix: conf

--- run_server.sh
+++ run_server.sh
@@ -1,1 +1,19 @@
+sudo systemctl daemon-reload
+
 dart bin/main.dart --mode production --server-id 2
+
+sudo systemctl status fixie.socket
+
+sudo rm /etc/nginx/certificate.crt
+sudo rm /etc/nginx/private.key
+
+sudo touch /etc/nginx/certificate.crt
+sudo touch /etc/nginx/private.key
+
+sudo cp nginx.conf /etc/nginx/nginx.conf
+sudo cp certificate.crt /etc/nginx/certificate.crt
+sudo cp private.key /etc/nginx/private.key
+
+sudo systemctl restart nginx
+sudo systemctl status nginx
+
