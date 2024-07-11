ba3c0c817824b6d3e3e025bbd3523b5e5e9c63f0
Tanguy De Brabandere <125645329+TanguyDeBrabandere@users.noreply.github.com>
2024-07-11 14:17:57 +0200

    Update run_server.sh

--- run_server.sh
+++ run_server.sh
@@ -8,9 +8,9 @@ sudo cp nginx.conf /etc/nginx/nginx.conf
 sudo cp certificate.crt /etc/nginx/certificate.crt
 sudo cp private.key /etc/nginx/private.key
 
-docker run $(docker build -q .) 8080:8080
-
 sudo systemctl daemon-reload
 
 sudo systemctl restart nginx
 sudo systemctl status nginx
+
+docker run $(docker build -q .) 8080:8080
