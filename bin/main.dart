08a4809efd6fb0e6e367a0ea9ca9295ecdf134a8
Tanguy De Brabandere <125645329+TanguyDeBrabandere@users.noreply.github.com>
2024-07-11 10:33:38 +0200

    fix: flutter -> dart

--- bin/main.dart
+++ bin/main.dart
@@ -1,19 +1,16 @@
 import 'package:fixie_server/server.dart';
-import 'package:sentry_flutter/sentry_flutter.dart';
+import 'package:sentry/sentry.dart';
 
 /// This is the starting point for your Serverpod server. Typically, there is
 /// no need to modify this file.
 void main(List<String> args) {
-  SentryFlutter.init(
+  Sentry.init(
     (options) {
       options.dsn =
           'https://fa2b7c080a4ff33fe2aa52e6e30df08b@o4506789659475968.ingest.us.sentry.io/4507582210179072';
       // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
       // We recommend adjusting this value in production.
       options.tracesSampleRate = 1.0;
-      // The sampling rate for profiling is relative to tracesSampleRate
-      // Setting to 1.0 will profile 100% of sampled transactions:
-      options.profilesSampleRate = 1.0;
     },
     appRunner: () => run(args),
   );
