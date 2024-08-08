7930d1807cfae18e354286d2741200eb2c13828f
Tanguy De Brabandere <125645329+TanguyDeBrabandere@users.noreply.github.com>
2024-08-08 14:24:18 +0200

    Update run_server.sh

--- run_server.sh
+++ run_server.sh
@@ -3,7 +3,7 @@ docker build -t fixie_server .
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 
-docker run -dp 127.0.0.1:8080:8080 fixie_server
+docker run -d -p 8080:8080 -p 8081:8081 fixie_server
 
 sudo rm /etc/nginx/certificate.crt
 sudo rm /etc/nginx/private.key
