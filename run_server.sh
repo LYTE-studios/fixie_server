94562c2fc1030933dfab771f068c9f2a3a0d3496
Tanguy De Brabandere <125645329+TanguyDeBrabandere@users.noreply.github.com>
2024-09-24 08:32:01 +0200

    Update run_server.sh

--- run_server.sh
+++ run_server.sh
@@ -7,8 +7,8 @@ sudo touch /etc/nginx/certificate.crt
 sudo touch /etc/nginx/private.key
 
 sudo cp nginx.conf /etc/nginx/nginx.conf
-sudo cp certificate.crt /etc/nginx/certificate.crt
-sudo cp private.key /etc/nginx/private.key
+sudo cp ../certificate.crt /etc/nginx/certificate.crt
+sudo cp ../private.key /etc/nginx/private.key
 
 sudo systemctl daemon-reload
 
